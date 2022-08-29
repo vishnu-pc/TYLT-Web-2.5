588ff430d982b9360d608393cbe996bc2366a825
vishnu-pc <vishnupc1999@gmail.com>
2022-08-29 19:31:55 +0530

    routing setup

---
+++ src/components/landing-components/dynamicCanvas.js
@@ -0,0 +1,10 @@
+export function dynamicCanvas() {
+
+    return (
+        <div className='dynamicCanvasElement'>
+            Dynamic Canvas
+        </div>
+    );
+}
+
+export default dynamicCanvas;
---
+++ src/components/landingPage.js
@@ -0,0 +1,13 @@
+import DynamicCanvas from './landing-components/dynamicCanvas';
+
+export function landingPage() {
+
+    return (
+        <div className='landingPageElement'>
+            <DynamicCanvas /> 
+            Next
+        </ div>
+    );
+}
+
+export default landingPage;
